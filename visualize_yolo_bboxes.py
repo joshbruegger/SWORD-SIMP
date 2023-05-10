d7db12c12f98e8ee1c3443266033c585b48304ac
Josh Bruegger <josh@bruegger.it>
2023-05-10 12:52:36 +0200

    refactor: :art: autoformat

--- visualize_yolo_bboxes.py
+++ visualize_yolo_bboxes.py
@@ -4,6 +4,7 @@ import cv2
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
+
 def visualize_yolo_bboxes(image_path, output_path=None):
     # Check if the .txt file exists
     txt_path = os.path.splitext(image_path)[0] + '.txt'
@@ -32,7 +33,8 @@ def visualize_yolo_bboxes(image_path, output_path=None):
         w = w * width
         h = h * height
 
-        rect = patches.Rectangle((x, y), w, h, linewidth=2, edgecolor='r', facecolor='none')
+        rect = patches.Rectangle(
+            (x, y), w, h, linewidth=2, edgecolor='r', facecolor='none')
         ax.add_patch(rect)
         ax.text(x, y-5, str(int(class_id)), fontsize=12, color='r')
 
@@ -42,13 +44,17 @@ def visualize_yolo_bboxes(image_path, output_path=None):
     else:
         plt.show()
 
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Visualize YOLO bounding boxes")
+    parser = argparse.ArgumentParser(
+        description="Visualize YOLO bounding boxes")
     parser.add_argument("image_path", help="Path to the .png image file")
-    parser.add_argument("-o", "--output", dest="output_path", nargs='?', const=True, help="Path to save the visualization image or flag to save with default name", default=None)
+    parser.add_argument("-o", "--output", dest="output_path", nargs='?', const=True,
+                        help="Path to save the visualization image or flag to save with default name", default=None)
     args = parser.parse_args()
 
     if args.output_path is True:
-        args.output_path = os.path.join(os.path.dirname(args.image_path), f"visualization_{os.path.basename(args.image_path)}")
+        args.output_path = os.path.join(os.path.dirname(
+            args.image_path), f"visualization_{os.path.basename(args.image_path)}")
 
     visualize_yolo_bboxes(args.image_path, args.output_path)
