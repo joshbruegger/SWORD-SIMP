0fe13df1a51485449a9671c22aab1e3b3e555da7
Josh Bruegger <josh@bruegger.it>
2023-06-02 20:30:39 +0100

    Add bbox generation to setup

--- setup.sh
+++ setup.sh
@@ -5,7 +5,10 @@ pip install --upgrade pip
 pip install -r requirements.txt
 
 # download dataset using the download script
-python download.py dataset/source/
+python download.py dataset/source/images
+
+# generate the bboxes
+python generate_bboxes.py dataset/source/images -o dataset/source/labels
 
 # generate crops
 python generate_crops.py dataset/source/ 2
