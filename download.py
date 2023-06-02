8bf35158c441f6840ce6c0f3aff7ca0e9c1f60fe
Josh Bruegger <josh@bruegger.it>
2023-06-02 19:19:56 +0100

    feat: :sparkles: Added cli functionality to download script

--- download.py
+++ download.py
@@ -1,4 +1,17 @@
 import gdown
+import os
+import argparse
 
-url = "https://drive.google.com/drive/folders/1Ouga4ms22NK-sDUkI4MoFqHuhxG2qG1i"
-gdown.download_folder(url, use_cookies=False, output="dataset/source/images")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('output', help='Download directory')
+    args = parser.parse_args()
+
+    if os.path.exists(args.output):
+        print("fatal: output directory already exists. Already downloaded?")
+        exit()
+
+    url = "https://drive.google.com/drive/folders/1Ouga4ms22NK-sDUkI4MoFqHuhxG2qG1i"
+    gdown.download_folder(url, use_cookies=False,
+                          output=args.output, quiet=False)
