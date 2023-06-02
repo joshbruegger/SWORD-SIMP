3d514671ed479b1c6e7f4ad05182460215d60291
Josh Bruegger <josh@bruegger.it>
2023-06-02 19:21:23 +0100

    refactor: :recycle: updated setup script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
+#! /bin/sh
+
 # make sure the requirements are installed
+pip install --upgrade pip
 pip install -r requirements.txt
 
 # download dataset using the download script
-python download.py
+python download.py dataset/source/
 
 # generate crops
-python generate_crops.py
+python generate_crops.py dataset/source/ 2
