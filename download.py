56728bc0495b8bbcc5e85511b6f933d0b05a29c7
Josh Bruegger <josh@bruegger.it>
2023-06-02 14:30:53 +0100

    refactor: :recycle: Update download script to use download_folder method for downloading files from a shared link.

--- download.py
+++ download.py
@@ -1,9 +1,4 @@
 import gdown
 
-to_download = [
-    ["Giovanni_di_Nicola_Madonna_in_trono_col_Bambino_e_donatore.png",
-        "https://drive.google.com/file/d/10t4zLgSG4YY1XMWREjmmX4Vlr64AeJMA/view?usp=share_link"],
-]
-
-for output, url in to_download:
-    gdown.download(url=url, output=output, quiet=False, fuzzy=True)
+url = "https://drive.google.com/drive/folders/1Ouga4ms22NK-sDUkI4MoFqHuhxG2qG1i"
+gdown.download_folder(url, use_cookies=False, output="dataset/source")
