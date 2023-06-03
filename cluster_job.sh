68fa872dfc67eb90216c13abd5d455e7fdc7030b
Josh Bruegger <josh@bruegger.it>
2023-06-03 08:31:33 +0100

    refactor: :recycle: Updated requirements, updated job

--- cluster_job.sh
+++ cluster_job.sh
@@ -12,6 +12,12 @@ module purge
 # Load the Python version that has been used to construct the virtual environment
 module load PyTorch/1.12.1-foss-2022a-CUDA-11.7.0
 
+# Check if the virtual environment exists
+if [ ! -d "$HOME/.envs/thesis_env" ]; then
+    # Create the virtual environment
+    python3 -m venv $HOME/.envs/thesis_env
+fi
+
 # Activate the virtual environment
 source $HOME/.envs/thesis_env/bin/activate
 
