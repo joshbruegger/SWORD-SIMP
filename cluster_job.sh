6aa1c60f5876882d163defa01175e817f61de3fd
Josh Bruegger <josh@bruegger.it>
2023-06-03 20:52:57 +0100

    perf: :zap: Added correct modules, made cluster job work on scratch disk

--- cluster_job.sh
+++ cluster_job.sh
@@ -10,7 +10,13 @@
 # Clear the module environment
 module purge
 # Load the Python version that has been used to construct the virtual environment
-module load PyTorch/1.12.1-foss-2022a-CUDA-11.7.0
+module load PyTorch/1.12.1-foss-2022a-CUDA-11.7.0 #3.10.4
+module load OpenCV/4.6.0-foss-2022a-contrib #3.10.4
+
+# Save working directory in variable
+WORKDIR=$SLURM_SUBMIT_DIR
+
+cd $TMPDIR
 
 # Check if the virtual environment exists
 if [ ! -d "$HOME/.envs/thesis_env" ]; then
@@ -21,8 +27,17 @@ fi
 # Activate the virtual environment
 source $HOME/.envs/thesis_env/bin/activate
 
-# Run the setup script
-chmod +x setup.sh
-bash setup.sh
+# make sure the requirements are installed
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# download dataset using the download script in work dir
+python $WORKDIR/download.py $SCRATCH/dataset/source/images
+
+# generate the bboxes
+python $WORKDIR/generate_bboxes.py $SCRATCH/dataset/source/images -o $SCRATCH/dataset/source/labels
+
+# generate crops
+python $WORKDIR/generate_crops.py $SCRATCH/dataset/source/ 10
 
 deactivate
