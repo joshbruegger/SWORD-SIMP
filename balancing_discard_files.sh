e0cb3139489bf84f051590369f12faf761b106cc
Josh Bruegger <josh@bruegger.it>
2023-07-15 18:27:09 +0200

    Fix shell file for balancing in accorance to previous commit

--- balancing_discard_files.sh
+++ balancing_discard_files.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=thesis
-#SBATCH --output=balancing_analyze-%j.log
+#SBATCH --output=balancing_discard_files-%j.log
 #SBATCH --mem=16G
-#SBATCH --time=01:00:00
+#SBATCH --time=00:10:00
 
-python -u balancing_discard_files.py balancing_files_to_discard.txt
+module purge
+module load Python/3.10.4-GCCcore-11.3.0
+
+python -u balancing_discard_files.py balancing_to_discard_out.txt
