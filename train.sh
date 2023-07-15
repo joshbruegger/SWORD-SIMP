91e457a5d4a1b7898e43ef5b6a9c3eca69174195
Josh Bruegger <josh@bruegger.it>
2023-07-15 16:20:42 +0200

    Removed flag parsing because it was acting up and i didnt wanna fix it, made memory and time requirements better

--- train.sh
+++ train.sh
@@ -2,29 +2,22 @@
 #SBATCH --gpus-per-node=a100:1
 #SBATCH --job-name=thesis_train
 #SBATCH --output=train-%j.log
-#SBATCH --time=00:10:00
-#SBATCH --mem=4G
+#SBATCH --time=03:30:00
+#SBATCH --mem=10G
 
 # Help function
 function usage {
     echo "Usage: $0 [-e <number>]"po
     echo "  -e <number>: number of epochs (default = 10)"
+    echo "  -b <number>: batch size (default = 32)"
     exit 1
 }
 
 # Process flags
-e=10
-while getopts ":e:" opt; do
-    case $opt in
-        n) n="$OPTARG"
-            if ! [[ "$e" =~ ^[0-9]+$ ]] ; then
-                echo "error: -e argument is not a number" >&2
-                usage
-            fi;;
-        \?) echo "Invalid option -$OPTARG" >&2
-            usage;;
-    esac
-done
+e=1
+b=32
+echo "Epochs: $e"
+echo "Batch size: $b"
 
 WORKDIR=$(pwd)
 SCRATCH=/scratch/$USER
@@ -33,6 +26,6 @@ SCRATCH=/scratch/$USER
 chmod +x $WORKDIR/setup_env.sh
 source $WORKDIR/setup_env.sh
 
-python3 -u train.py -d $SCRATCH/dataset -c $SCRATCH/dataset/classes.txt -e $e
+python3 -u train.py -d $SCRATCH/dataset -c $SCRATCH/dataset/classes.txt -e $e -b $b
 
 deactivate
