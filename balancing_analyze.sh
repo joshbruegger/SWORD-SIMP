3da999e5eba29ce231596b0c512b6e99ad4f3f5e
Josh Bruegger <josh@bruegger.it>
2023-07-18 15:52:04 +0200

    Woops, didn't mean to change this

--- balancing_analyze.sh
+++ balancing_analyze.sh
@@ -2,10 +2,10 @@
 #SBATCH --job-name=thesis
 #SBATCH --output=balancing_analyze-%j.log
 #SBATCH --mem=16G
-#SBATCH --time=00:10:00
+#SBATCH --time=04:00:00
 
 source setup_env.sh
 
-python -u balancing_analyze.py /scratch/s4361687/small
+python -u balancing_analyze.py /scratch/s4361687/dataset
 
 deactivate
