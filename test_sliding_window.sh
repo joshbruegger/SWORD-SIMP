e77e0d5d9ed7d1d0348acb50ac09aadb8acf282c
Josh Bruegger <josh@bruegger.it>
2023-08-05 21:37:41 +0200

    added debug flag to job

--- test_sliding_window.sh
+++ test_sliding_window.sh
@@ -2,9 +2,9 @@
 #SBATCH --job-name=sliding_window
 #SBATCH --output=sliding_window-%j.log
 #SBATCH --gpus-per-node=1
-#SBATCH --time=00:10:00
-#SBATCH --mem=20G
+#SBATCH --time=00:15:00
+#SBATCH --mem=30G
 
 source setup_env.sh
 
-python -u sliding_window.py /scratch/s4361687/dataset/test/images/07_Traino_S_Domenico_2.psb checkpoints/run_300-400cyclic/ckpt_best.pth 28 -s 1080 -g /scratch/s4361687/dataset/test/labels/07_Traino_S_Domenico_2.txt -y /scratch/s4361687/dataset/dataset.yaml -v
+python -u sliding_window.py /scratch/s4361687/dataset/test/images/07_Traino_S_Domenico_2.psb checkpoints/run_300-400cyclic/ckpt_best.pth 28 -s 1080 -g /scratch/s4361687/dataset/test/labels/07_Traino_S_Domenico_2.txt -y /scratch/s4361687/dataset/dataset.yaml -v -d
